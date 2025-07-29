ffedf5eb8273e1283776bb52708ebf9803cf7182
tinahrabe <tinahrabe602@gmail.com>
2025-07-29 16:47:40 +0300

    commencement page a propos

--- animation.js
+++ animation.js
@@ -11,3 +11,14 @@ texte.addEventListener("mouseenter", () => {
 texte.addEventListener("mouseleave", () => {
     texte.style.animationPlayState = "running";
 });
+
+window.addEventListener('scroll', function() {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.remove('transparent');
+    navbar.classList.add('solid');
+  } else {
+    navbar.classList.remove('solid');
+    navbar.classList.add('transparent');
+  }
+});
